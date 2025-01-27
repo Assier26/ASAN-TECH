29645aded3c3df1db196e0a1f6cd2e66910fb126
AndresSierra-Oficial <andres.sierra.sanchez@outlook.com>
2025-01-27 19:15:37 +0100

    Create instalacionVbox.sh
    Instalación de vbox y carpeta compartida en /mnt/carp_comp

---
+++ instalacionInicial.sh
@@ -0,0 +1,22 @@
+#Actualizar 
+apt update && apt upgrade -y
+
+# --	Instalación Vbox	--
+#Instalar dependencias Vbox
+apt install build-essential dkms linux-headers-$(uname -r)
+#Insertamos el cd en la maquina virtual
+# Montamos el cd en /mnt
+mount /dev/cdrom /mnt
+# Ejecutamos el programa
+/mnt/VBoxLinuxAdditions.run
+
+
+# --	Carpeta Compartida (carp_comp)	--
+# Crear el directorio compartido
+mkdir /mnt/carp_com
+# Crear el montaje persistente en /etc/fstab.
+echo "carp_com   /mnt/carp_com   vboxsf   defaults   0   0" > /etc/fstab
+
+
+# --	Reiniciamos	--
+reboot
