3ca2ab7c13e87d6b5b31ac8f15ca7d97644f639e
AndresSierra-Oficial <andres.sierra.sanchez@outlook.com>
2025-03-18 00:11:44 +0100

    m

--- 03_ser_app/zz_Pre-ProyectoExterno/ansible/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ejecutamos el playbook de despliegue de Kubernetes
-ansible-playbook -i hosts despliegue-kubernetes.yml
-
-# Ejecutamos el playbook de despliegue de la aplicación
-ansible-playbook -i hosts despliegue-aplicacion.yml
